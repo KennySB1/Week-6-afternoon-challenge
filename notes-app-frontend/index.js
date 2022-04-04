be29dfa1094ed9c2f01c34f85f971ab89ff32fd4
Kenny Sweeney-Baird <kenny@Kennys-MacBook-Pro-2.local>
2022-04-04 15:23:25 +0100

    connected notes model to the webpage

--- notes-app-frontend/index.js
+++ notes-app-frontend/index.js
@@ -1,1 +1,5 @@
 console.log('The notes app is running')
+const NotesModel = require('./notesModel')
+const notesModel = new NotesModel()
+notesModel.addNote('testing')
+console.log(notesModel.getNotes())
